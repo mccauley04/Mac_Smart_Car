97232c4af52a0c6026fce45d87b71fd4c9c3f6a4
mac_4_dwight <mccaul38@msu.edu>
2019-01-13 20:31:10 -0600

    Self Practice 1

--- Code/Color_LED/ColorLED.py
+++ Code/Color_LED/ColorLED.py
@@ -11,6 +11,13 @@ LED_B = 24
 #Set the GPIO port to BCM encoding mode.
 GPIO.setmode(GPIO.BCM)
 
+#Definition of Ultrasonic Module Pins
+EchoPin = 0
+TrigPin = 1
+
+#Definition of servo pin
+ServoPin = 23
+
 #RGB pins are initialized into output mode
 global pwm_ENA
 global pwm_ENB
@@ -22,12 +29,6 @@ GPIO.setup(ServoPin, GPIO.OUT)
 pwm_servo = GPIO.PWM(ServoPin, 50)
 pwm_servo.start(0)
 
-#Definition of Ultrasonic Module Pins
-EchoPin = 0
-TrigPin = 1
-
-#Definition of servo pin
-ServoPin = 23
 
 #The servo rotates to the specified angle
 def servo_appointed_detection(pos):
