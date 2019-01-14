31bdfe2bc3989b337430bbaba5a176fce3ce6071
mac_4_dwight <mccaul38@msu.edu>
2019-01-14 15:44:08 -0600

    Self Practice 1

--- Code/Path_Finder.py
+++ Code/Path_Finder.py
@@ -176,45 +176,103 @@ def servo_color_carstate():
     GPIO.output(LED_R, GPIO.HIGH)
     GPIO.output(LED_G, GPIO.LOW)
     GPIO.output(LED_B, GPIO.LOW)
-    back(20, 20)
-    time.sleep(0.08)
+
+    'back(20, 20)' 'Not sure why we move the car before Analyzing the suroundings'
+
     brake()
+    'Apply the break to make sure the car is not moving during this time'
+
+    time.sleep(0.08)
 
     servo_appointed_detection(0)
-    time.sleep(0.8)
-    rightdistance = Distance_test()
+    time.sleep(0.25)
+    'First line of code had a time.sleep state of 0.8'
+    scan_distance_0 = Distance_test()
 
-    servo_appointed_detection(180)
-    time.sleep(0.8)
-    leftdistance = Distance_test()
+    servo_appointed_detection(30)
+    time.sleep(0.25)
+    scan_distance_30 = Distance_test()
+
+    rside_distance = scan_distance_0 + scan_distance_30
+
+    servo_appointed_detection(60)
+    time.sleep(0.25)
+    scan_distance_60 = Distance_test()
 
     servo_appointed_detection(90)
-    time.sleep(0.8)
-    frontdistance = Distance_test()
+    time.sleep(0.25)
+    scan_distance_90 = Distance_test()
 
+    rmiddle_distance = scan_distance_60 + scan_distance_90
+    rdistance = rmiddle_distance + rside_distance
 
-    if leftdistance < 30 and rightdistance < 30 and frontdistance < 30:
-        'Magenta'
-        GPIO.output(LED_R, GPIO.HIGH)
-        GPIO.output(LED_G, GPIO.LOW)
-        GPIO.output(LED_B, GPIO.HIGH)
-        spin_right(35, 35)
-        time.sleep(0.58)
+    servo_appointed_detection(90)
+    time.sleep(0.25)
+    scan_distance_90 = Distance_test()
 
-    elif leftdistance >= rightdistance:
+    servo_appointed_detection(120)
+    time.sleep(0.25)
+    scan_distance_120 = Distance_test()
+
+    lmiddle_distance = scan_distance_90 + scan_distance_120
+
+    servo_appointed_detection(150)
+    time.sleep(0.25)
+    scan_distance_150 = Distance_test()
+
+    servo_appointed_detection(180)
+    time.sleep(0.25)
+    scan_distance_180 = Distance_test()
+
+    lside_distance = scan_distance_150 + scan_distance_180
+    ldistance = lside_distance + lmiddle_distance
+
+    if lside_distance < 35 and lmiddle_distance < 35 and rmiddle_distance < 35 and rside_distance < 35:
+
+    'if leftdistance < 30 and rightdistance < 30 and frontdistance < 30:'
+
+    'Magenta'
+    GPIO.output(LED_R, GPIO.HIGH)
+    GPIO.output(LED_G, GPIO.LOW)
+    GPIO.output(LED_B, GPIO.HIGH)
+
+    'Slowly move back at tad and do a 180'
+    back(10, 10)
+    time.sleep(.58)
+
+    spin_right(25, 25)
+    time.sleep(0.58)
+
+    elif lside_distance >= lmiddle_distance:
+
+    'elif leftdistance >= rightdistance:'
         'Blue'
-        GPIO.output(LED_R, GPIO.LOW)
-        GPIO.output(LED_G, GPIO.LOW)
-        GPIO.output(LED_B, GPIO.HIGH)
-        spin_left(35, 35)
-        time.sleep(0.28)
-    elif leftdistance <= rightdistance:
-        'Magenta'
-        GPIO.output(LED_R, GPIO.HIGH)
-        GPIO.output(LED_G, GPIO.LOW)
-        GPIO.output(LED_B, GPIO.HIGH)
-        spin_right(35, 35)
-        time.sleep(0.28)
+    GPIO.output(LED_R, GPIO.LOW)
+    GPIO.output(LED_G, GPIO.LOW)
+    GPIO.output(LED_B, GPIO.HIGH)
+    spin_left(25, 25) '35,35'
+    time.sleep(0.28)
+
+    elif lmiddle_distance >= rmiddle_distance:
+
+    'elif leftdistance <= rightdistance:'
+        'Blue'
+    GPIO.output(LED_R, GPIO.Low)
+    GPIO.output(LED_G, GPIO.LOW)
+    GPIO.output(LED_B, GPIO.HIGH)
+    spin_left(25, 25)
+    '35,35'
+    time.sleep(0.28)
+
+    elif rmiddle_distance >= rside_distance:
+
+    'elif leftdistance <= rightdistance:'
+    'Blue'
+    GPIO.output(LED_R, GPIO.Low)
+    GPIO.output(LED_G, GPIO.LOW)
+    GPIO.output(LED_B, GPIO.HIGH)
+    spin_right(25, 25) '35,35'
+    time.sleep(0.28)
 
 
 # delay 2s
