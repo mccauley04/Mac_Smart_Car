3b4cfa7b3b0b9ab7fb65049c45ad2a3d13ba6a5f
mac_4_dwight <mccaul38@msu.edu>
2019-01-13 20:11:12 -0600

    Self Practice 1

--- Code/Color_LED/ColorLED.py
+++ Code/Color_LED/ColorLED.py
@@ -23,17 +23,17 @@ try:
         GPIO.output(LED_R, GPIO.HIGH)
         GPIO.output(LED_G, GPIO.LOW)
         GPIO.output(LED_B, GPIO.LOW)
-        time.sleep(1)
+        time.sleep(0.5)
         # Green for 1 Second
         GPIO.output(LED_R, GPIO.LOW)
         GPIO.output(LED_G, GPIO.HIGH)
         GPIO.output(LED_B, GPIO.LOW)
-        time.sleep(1)
+        time.sleep(0.5)
         # Blue for 1 Second
         GPIO.output(LED_R, GPIO.LOW)
         GPIO.output(LED_G, GPIO.LOW)
         GPIO.output(LED_B, GPIO.HIGH)
-        time.sleep(1)
+        time.sleep(0.5)
 
 except:
     print "except"
