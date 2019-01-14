bd0d26ec9a31925f613f5682f4a2835363ae9ec0
mac_4_dwight <mccaul38@msu.edu>
2019-01-13 21:18:35 -0600

    Self Practice 1

--- Code/Path_Finder.py
+++ Code/Path_Finder.py
@@ -1,9 +1,8 @@
-#-*- coding:UTF-8 -*-
+# -*- coding:UTF-8 -*-
 import RPi.GPIO as GPIO
 import time
 
-#Motor Pins
-
+# Definition of  motor pins
 IN1 = 20
 IN2 = 21
 IN3 = 19
@@ -11,67 +10,134 @@ IN4 = 26
 ENA = 16
 ENB = 13
 
-'''Definition of  key'''
+# Definition of  key
 key = 8
 
-'''Definition of  ultrasonic module pins'''
+# Definition of  ultrasonic module pins
 EchoPin = 0
 TrigPin = 1
 
-'''Definition of RGB module pins'''
+# Definition of RGB module pins
 LED_R = 22
 LED_G = 27
 LED_B = 24
 
-'''Definition of servo pin'''
+# Definition of servo pin
 ServoPin = 23
 
-'''Definition of infrared obstacle avoidance module pins'''
+# Definition of infrared obstacle avoidance module pins
 AvoidSensorLeft = 12
 AvoidSensorRight = 17
 
-'''Set the GPIO port to BCM encoding mode'''
+# Set the GPIO port to BCM encoding mode
 GPIO.setmode(GPIO.BCM)
 
-'''Ignore warning information'''
+# Ignore warning information
 GPIO.setwarnings(False)
 
-'''
-Motor pins are initialized into output mode
-Key pin is initialized into input mode
-Ultrasonic pin,RGB pin,servo pin initialization
-Infrared obstacle avoidance module pin
-'''
 
-def pre_check():
+# Motor pins are initialized into output mode
+# Key pin is initialized into input mode
+# Ultrasonic pin,RGB pin,servo pin initialization
+# infrared obstacle avoidance module pin
+def init():
     global pwm_ENA
     global pwm_ENB
     global pwm_servo
-    GPIO.setup(ENA,GPIO.OUT,initial=GPIO.HIGH)
-    GPIO.setup(IN1,GPIO.OUT,initial=GPIO.LOW)
-    GPIO.setup(IN2,GPIO.OUT,initial=GPIO.LOW)
-    GPIO.setup(ENB,GPIO.OUT,initial=GPIO.HIGH)
-    GPIO.setup(IN3,GPIO.OUT,initial=GPIO.LOW)
-    GPIO.setup(IN4,GPIO.OUT,initial=GPIO.LOW)
-    GPIO.setup(key,GPIO.IN)
-    GPIO.setup(EchoPin,GPIO.IN)
-    GPIO.setup(TrigPin,GPIO.OUT)
+    GPIO.setup(ENA, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(IN1, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(IN2, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(ENB, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(IN3, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(IN4, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(key, GPIO.IN)
+    GPIO.setup(EchoPin, GPIO.IN)
+    GPIO.setup(TrigPin, GPIO.OUT)
     GPIO.setup(LED_R, GPIO.OUT)
     GPIO.setup(LED_G, GPIO.OUT)
     GPIO.setup(LED_B, GPIO.OUT)
     GPIO.setup(ServoPin, GPIO.OUT)
-    GPIO.setup(AvoidSensorLeft,GPIO.IN)
-    GPIO.setup(AvoidSensorRight,GPIO.IN)
-    '''Set the PWM pin and frequency is 2000hz'''
+    GPIO.setup(AvoidSensorLeft, GPIO.IN)
+    GPIO.setup(AvoidSensorRight, GPIO.IN)
+    # Set the PWM pin and frequency is 2000hz
     pwm_ENA = GPIO.PWM(ENA, 2000)
     pwm_ENB = GPIO.PWM(ENB, 2000)
     pwm_ENA.start(0)
     pwm_ENB.start(0)
 
-pwm_servo = GPIO.PWM(ServoPin, 50)
-pwm_servo.start(0)
+    pwm_servo = GPIO.PWM(ServoPin, 50)
+    pwm_servo.start(0)
 
-#Button detection
+
+# advance
+def run(leftspeed, rightspeed):
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(leftspeed)
+    pwm_ENB.ChangeDutyCycle(rightspeed)
+
+
+# back
+def back(leftspeed, rightspeed):
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.HIGH)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.HIGH)
+    pwm_ENA.ChangeDutyCycle(leftspeed)
+    pwm_ENB.ChangeDutyCycle(rightspeed)
+
+
+# turn left
+def left(leftspeed, rightspeed):
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(leftspeed)
+    pwm_ENB.ChangeDutyCycle(rightspeed)
+
+
+# trun right
+def right(leftspeed, rightspeed):
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(leftspeed)
+    pwm_ENB.ChangeDutyCycle(rightspeed)
+
+
+# turn left in place
+def spin_left(leftspeed, rightspeed):
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.HIGH)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(leftspeed)
+    pwm_ENB.ChangeDutyCycle(rightspeed)
+
+
+# turn right in place
+def spin_right(leftspeed, rightspeed):
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.HIGH)
+    pwm_ENA.ChangeDutyCycle(leftspeed)
+    pwm_ENB.ChangeDutyCycle(rightspeed)
+
+
+# brake
+def brake():
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.LOW)
+
+
+# Button detection
 def key_scan():
     while GPIO.input(key):
         pass
@@ -80,30 +146,34 @@ def key_scan():
         if not GPIO.input(key):
             time.sleep(0.01)
             while not GPIO.input(key):
-	        pass
+                pass
 
-#Ultrasonic function
+
+# Ultrasonic function
 def Distance_test():
-    GPIO.output(TrigPin,GPIO.HIGH)
+    GPIO.output(TrigPin, GPIO.HIGH)
     time.sleep(0.000015)
-    GPIO.output(TrigPin,GPIO.LOW)
+    GPIO.output(TrigPin, GPIO.LOW)
     while not GPIO.input(EchoPin):
         pass
     t1 = time.time()
     while GPIO.input(EchoPin):
         pass
     t2 = time.time()
-    print "distance is %d " % (((t2 - t1)* 340 / 2) * 100)
+    print
+    "distance is %d " % (((t2 - t1) * 340 / 2) * 100)
     time.sleep(0.01)
-    return ((t2 - t1)* 340 / 2) * 100
+    return ((t2 - t1) * 340 / 2) * 100
 
-#The servo rotates to the specified angle
+
+# The servo rotates to the specified angle
 def servo_appointed_detection(pos):
     for i in range(18):
-        pwm_servo.ChangeDutyCycle(2.5 + 10 * pos/180)
+        pwm_servo.ChangeDutyCycle(2.5 + 10 * pos / 180)
+
 
 def servo_color_carstate():
-    #red
+    # red
     GPIO.output(LED_R, GPIO.HIGH)
     GPIO.output(LED_G, GPIO.LOW)
     GPIO.output(LED_B, GPIO.LOW)
@@ -124,28 +194,29 @@ def servo_color_carstate():
     frontdistance = Distance_test()
 
     if leftdistance < 30 and rightdistance < 30 and frontdistance < 30:
-        #Magenta
+        # Magenta
         GPIO.output(LED_R, GPIO.HIGH)
         GPIO.output(LED_G, GPIO.LOW)
         GPIO.output(LED_B, GPIO.HIGH)
         spin_right(35, 35)
- 	time.sleep(0.58)
+        time.sleep(0.58)
     elif leftdistance >= rightdistance:
-	#Blue
-	GPIO.output(LED_R, GPIO.LOW)
+        # Blue
+        GPIO.output(LED_R, GPIO.LOW)
         GPIO.output(LED_G, GPIO.LOW)
         GPIO.output(LED_B, GPIO.HIGH)
-	spin_left(35, 35)
-	time.sleep(0.28)
+        spin_left(35, 35)
+        time.sleep(0.28)
     elif leftdistance <= rightdistance:
-	#Magenta
-	GPIO.output(LED_R, GPIO.HIGH)
+        # Magenta
+        GPIO.output(LED_R, GPIO.HIGH)
         GPIO.output(LED_G, GPIO.LOW)
         GPIO.output(LED_B, GPIO.HIGH)
-	spin_right(35, 35)
-	time.sleep(0.28)
+        spin_right(35, 35)
+        time.sleep(0.28)
 
-#delay 2s
+
+# delay 2s
 time.sleep(2)
 
 # The try/except statement is used to detect errors in the try block.
