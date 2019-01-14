73a4a8d8afbc87d28b5f3292827bb39868e7aabc
mac_4_dwight <mccaul38@msu.edu>
2019-01-13 19:33:24 -0600

    Self Practice 1

--- Code/Color_LED/ColorLED.c
+++ Code/Color_LED/ColorLED.c
@@ -1,13 +1,3 @@
-/**
-* @par Copyright (C): 2010-2019, Shenzhen Yahboom Tech
-* @file         ColorLED.c
-* @author       Danny
-* @version      V1.0
-* @date         2017.08.16
-* @brief        ColorLED
-* @details
-* @par History  
-*/
 #include <wiringPi.h>
 
 #define ON 1     
@@ -18,17 +8,6 @@ int LED_R = 3;           //LED_R is connected to  wiringPi port 3 of Raspberry p
 int LED_G = 2;           //LED_G is connected to  wiringPi port 2 of Raspberry pi
 int LED_B = 5;           //LED_B is connected to  wiringPi port 5 of Raspberry pi
 
-/**
-* Function       color_led
-* @author        Danny
-* @date          2017.08.16
-* @brief         7 different colors formed by different combinations of R,G and B
-* @param[in1]    Red
-* @param[in2]    Green
-* @param[in3]    Blue
-* @retval        void
-* @par History   
-*/
 void color_led(int v_iRed, int v_iGreen, int v_iBlue)
 {
   v_iRed == ON ? digitalWrite(LED_R, HIGH): digitalWrite(LED_R, LOW);
@@ -38,15 +17,6 @@ void color_led(int v_iRed, int v_iGreen, int v_iBlue)
   v_iBlue == ON ? digitalWrite(LED_B, HIGH) : digitalWrite(LED_B, LOW);
 }
 
-/**
-* Function       main
-* @author        Danny
-* @date          2017.08.16
-* @brief         Display 7 color LED
-* @param[in]     void
-* @retval        void
-* @par History   
-*/
 int main()
 {
 	wiringPiSetup();
