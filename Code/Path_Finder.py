0ed9f918ed85d121af85fbc244035b91fe0822a9
mac_4_dwight <mccaul38@msu.edu>
2019-01-16 18:43:07 -0600

    Self Practice 1

--- Code/Path_Finder.py
+++ Code/Path_Finder.py
@@ -35,11 +35,12 @@ GPIO.setmode(GPIO.BCM)
 'Ignore warning information'
 GPIO.setwarnings(False)
 
-
 'Motor pins are initialized into output mode'
 'Key pin is initialized into input mode'
 'Ultrasonic pin,RGB pin,servo pin initialization'
 'infrared obstacle avoidance module pin'
+
+
 def pre_checks():
     global pwm_ENA
     global pwm_ENB
@@ -69,86 +70,6 @@ def pre_checks():
     pwm_servo.start(0)
 
 
-'Advance'
-def run(leftspeed, rightspeed):
-    GPIO.output(IN1, GPIO.HIGH)
-    GPIO.output(IN2, GPIO.LOW)
-    GPIO.output(IN3, GPIO.HIGH)
-    GPIO.output(IN4, GPIO.LOW)
-    pwm_ENA.ChangeDutyCycle(leftspeed)
-    pwm_ENB.ChangeDutyCycle(rightspeed)
-
-
-'Back'
-def back(leftspeed, rightspeed):
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.HIGH)
-    GPIO.output(IN3, GPIO.LOW)
-    GPIO.output(IN4, GPIO.HIGH)
-    pwm_ENA.ChangeDutyCycle(leftspeed)
-    pwm_ENB.ChangeDutyCycle(rightspeed)
-
-
-'Turn left'
-def left(leftspeed, rightspeed):
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.LOW)
-    GPIO.output(IN3, GPIO.HIGH)
-    GPIO.output(IN4, GPIO.LOW)
-    pwm_ENA.ChangeDutyCycle(leftspeed)
-    pwm_ENB.ChangeDutyCycle(rightspeed)
-
-
-'Turn right'
-def right(leftspeed, rightspeed):
-    GPIO.output(IN1, GPIO.HIGH)
-    GPIO.output(IN2, GPIO.LOW)
-    GPIO.output(IN3, GPIO.LOW)
-    GPIO.output(IN4, GPIO.LOW)
-    pwm_ENA.ChangeDutyCycle(leftspeed)
-    pwm_ENB.ChangeDutyCycle(rightspeed)
-
-
-'Turn left in place'
-def spin_left(leftspeed, rightspeed):
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.HIGH)
-    GPIO.output(IN3, GPIO.HIGH)
-    GPIO.output(IN4, GPIO.LOW)
-    pwm_ENA.ChangeDutyCycle(leftspeed)
-    pwm_ENB.ChangeDutyCycle(rightspeed)
-
-
-'Turn right in place'
-def spin_right(leftspeed, rightspeed):
-    GPIO.output(IN1, GPIO.HIGH)
-    GPIO.output(IN2, GPIO.LOW)
-    GPIO.output(IN3, GPIO.LOW)
-    GPIO.output(IN4, GPIO.HIGH)
-    pwm_ENA.ChangeDutyCycle(leftspeed)
-    pwm_ENB.ChangeDutyCycle(rightspeed)
-
-
-'Break'
-def brake():
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.LOW)
-    GPIO.output(IN3, GPIO.LOW)
-    GPIO.output(IN4, GPIO.LOW)
-
-'Button detection'
-def key_scan():
-    while GPIO.input(key):
-        pass
-    while not GPIO.input(key):
-        time.sleep(0.01)
-        if not GPIO.input(key):
-            time.sleep(0.01)
-            while not GPIO.input(key):
-                pass
-
-
-'Ultrasonic function'
 def Distance_test():
     GPIO.output(TrigPin, GPIO.HIGH)
     time.sleep(0.000015)
@@ -165,10 +86,13 @@ def Distance_test():
     return ((t2 - t1) * 340 / 2) * 100
 
 
-'The servo rotates to the specified angle'
 def servo_appointed_detection(pos):
     for i in range(18):
         pwm_servo.ChangeDutyCycle(2.5 + 10 * pos / 180)
+
+
+angles = [0, 30, 60, 90, 120, 150, 180, 90]
+scan_dl = []
 
 
 def servo_color_carstate():
@@ -177,169 +101,34 @@ def servo_color_carstate():
     GPIO.output(LED_G, GPIO.LOW)
     GPIO.output(LED_B, GPIO.LOW)
 
-    'back(20, 20)' 'Not sure why we move the car before Analyzing the suroundings'
+    for index in range(8):
+        servo_appointed_detection(angles[index])
+        time.sleep(.4)
 
-    brake()
-    'Apply the break to make sure the car is not moving during this time'
-
-    time.sleep(0.08)
-
-    servo_appointed_detection(0)
-    time.sleep(0.25)
-    scan_distance_0 = Distance_test()
-    print(scan_distance_0)
-
-    servo_appointed_detection(30)
-    time.sleep(0.25)
-    scan_distance_30 = Distance_test()
-    print(scan_distance_30)
-
-    rside_distance = scan_distance_0 + scan_distance_30
-    print(rside_distance)
-
-    servo_appointed_detection(60)
-    time.sleep(0.25)
-    scan_distance_60 = Distance_test()
-    print(scan_distance_60)
-
-    servo_appointed_detection(90)
-    time.sleep(0.25)
-    scan_distance_90 = Distance_test()
-    print(scan_distance_90)
-
-    rmiddle_distance = scan_distance_60 + scan_distance_90
-    print(rmiddle_distance)
-    rdistance = rmiddle_distance + rside_distance
-    print(rdistance)
-
-    servo_appointed_detection(120)
-    time.sleep(0.25)
-    scan_distance_120 = Distance_test()
-    print(scan_distance_120)
-
-    lmiddle_distance = scan_distance_90 + scan_distance_120
-    print(lmiddle_distance)
-
-    servo_appointed_detection(150)
-    time.sleep(0.25)
-    scan_distance_150 = Distance_test()
-    print(scan_distance_150)
-
-    servo_appointed_detection(180)
-    time.sleep(0.25)
-    scan_distance_180 = Distance_test()
-    print(scan_distance_180)
-
-    lside_distance = scan_distance_150 + scan_distance_180
-    print(lside_distance)
-
-    ldistance = lside_distance + lmiddle_distance
-    print(ldistance)
-
-    if ldistance < 35 and rdistance < 35:
-
-    # if leftdistance < 30 and rightdistance < 30 and frontdistance < 30: 'Magenta'
-        GPIO.output(LED_R, GPIO.HIGH)
-        GPIO.output(LED_G, GPIO.LOW)
-        GPIO.output(LED_B, GPIO.HIGH)
-
-        spin_right(25, 25)
-        time.sleep(0.58)
-
-    elif lside_distance >= lmiddle_distance:
-
-        # elif leftdistance >= rightdistance: 'Blue'
-        GPIO.output(LED_R, GPIO.LOW)
-        GPIO.output(LED_G, GPIO.LOW)
-        GPIO.output(LED_B, GPIO.HIGH)
-        spin_left(25, 25) #35,35
-        time.sleep(0.28)
-
-    elif lmiddle_distance >= rmiddle_distance:
-
-    # elif leftdistance <= rightdistance:
-        'Blue'
-        GPIO.output(LED_R, GPIO.LOW)
-        GPIO.output(LED_G, GPIO.LOW)
-        GPIO.output(LED_B, GPIO.HIGH)
-        spin_left(25, 25) #35,35
-        time.sleep(0.28)
-
-    elif rmiddle_distance >= rside_distance:
-
-        # elif leftdistance <= rightdistance:
-        'Blue'
-        GPIO.output(LED_R, GPIO.Low)
-        GPIO.output(LED_G, GPIO.LOW)
-        GPIO.output(LED_B, GPIO.HIGH)
-        spin_right(25, 25) #35,35
-        time.sleep(0.28)
+        scan_dl.append(int(round(Distance_test(), 2)))
+        print(scan_dl)
 
 
-# delay 2s
-time.sleep(2)
+def decision_time():
+    'YELLOW'
 
-'''The try/except statement is used to detect errors in the try block.
-The except statement catches the exception information and processes it.'''
+    right_side = scan_dl[0] + scan_dl[1] + scan_dl[2] + scan_dl[3]
+    left_side = scan_dl[3] + scan_dl[4] + scan_dl[5] + scan_dl[6]
+
+    print(right_side)
+    print(left_side)
+
+    if right_side > left_side:
 
 
 try:
     pre_checks()
-    key_scan()
-    while True:
-        distance = Distance_test()
-        if distance > 50:
-
-            'There is obstacle, the indicator light of the infrared obstacle avoidance module is on, and the port level is LOW'
-            'There is no obstacle, the indicator light of the infrared obstacle avoidance module is off, and the port level is HIGH'
-
-
-            LeftSensorValue = GPIO.input(AvoidSensorLeft)
-            RightSensorValue = GPIO.input(AvoidSensorRight)
-
-            if LeftSensorValue == True and RightSensorValue == True:
-                run(50, 50)
-            elif LeftSensorValue == True and RightSensorValue == False:
-                spin_left(35, 35)
-                time.sleep(0.002)
-            elif RightSensorValue == True and LeftSensorValue == False:
-                spin_right(35, 35)
-                time.sleep(0.002)
-            elif RightSensorValue == False and LeftSensorValue == False:
-                spin_right(35, 35)
-                time.sleep(0.002)
-                run(100, 100)
-
-                GPIO.output(LED_R, GPIO.LOW)
-                GPIO.output(LED_G, GPIO.HIGH)
-                GPIO.output(LED_B, GPIO.LOW)
-
-        elif 30 <= distance <= 50:
-
-            'There is obstacle, the indicator light of the infrared obstacle avoidance module is on, and the port level is LOW'
-            'There is no obstacle, the indicator light of the infrared obstacle avoidance module is off, and the port level is HIGH'
-
-            LeftSensorValue = GPIO.input(AvoidSensorLeft)
-            RightSensorValue = GPIO.input(AvoidSensorRight)
-
-            if LeftSensorValue == True and RightSensorValue == True:
-                run(50, 50)
-            elif LeftSensorValue == True and RightSensorValue == False:
-                spin_left(35, 35)
-                time.sleep(0.002)
-            elif RightSensorValue == True and LeftSensorValue == False:
-                spin_right(35, 35)
-                time.sleep(0.002)
-            elif RightSensorValue == False and LeftSensorValue == False:
-                spin_right(35, 35)
-                time.sleep(0.002)
-                run(50, 50)
-
-        elif distance < 30:
-            servo_color_carstate()
+    servo_color_carstate()
+    decision_time()
 
 except KeyboardInterrupt:
     pass
+
 pwm_ENA.stop()
 pwm_ENB.stop()
 GPIO.cleanup()
