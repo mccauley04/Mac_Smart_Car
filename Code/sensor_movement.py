b1d1901c0610fcac256cd1ff9f0396091c48e70b
mac_4_dwight <mccaul38@msu.edu>
2019-01-16 18:25:03 -0600

    Self Practice 1

--- Code/sensor_movement.py
+++ Code/sensor_movement.py
@@ -100,7 +100,7 @@ def servo_color_carstate():
         servo_appointed_detection(angles[index])
         time.sleep(1)
 
-        scan_distance_list.append(str(round(Distance_test())))
+        scan_distance_list.append(str(round(Distance_test(),2)))
         print(scan_distance_list)
 
 
